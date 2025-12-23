f48c07c6424b9fc7eed634f4567b99675c9c2105
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-12-24 01:07:19 +0300

    Add modules directory to Tailwind content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config = {
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/modules/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     container: {
